4b449d6721e311b2031b6715c8c2c9484b3b7260
Matheus Amaral da Costa <161625293+MatheusADC@users.noreply.github.com>
2026-01-19 09:47:27 -0300

    feat: Others methods
    - post
    - put
    - delete

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { Pool } from 'pg';
+import express from "express";
+import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
@@ -9,9 +9,84 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 
 app.use(express.json());
 
-app.get('/books', async (req, res) => {
-  const { rows } = await pool.query('SELECT * FROM books');
+app.get("/books", async (req, res) => {
+  const { author_id, publisher_id, genre_id } = req.query;
+
+  const conditions = [];
+  const values = [];
+
+  if (author_id) {
+    conditions.push(`author_id = $${values.length + 1}`);
+    values.push(author_id);
+  }
+
+  if (publisher_id) {
+    conditions.push(`publisher_id = $${values.length + 1}`);
+    values.push(publisher_id);
+  }
+
+  if (genre_id) {
+    conditions.push(`genre_id = $${values.length + 1}`);
+    values.push(genre_id);
+  }
+
+  const whereClause =
+    conditions.length > 0 ? `WHERE ${conditions.join(" AND ")}` : "";
+  const query = `SELECT * FROM books ${whereClause}`;
+
+  const { rows } = await pool.query(query, values);
   res.json(rows);
 });
 
-app.listen(3000, () => console.log('API rodando na porta 3000'));
+app.post("/books", async (req, res) => {
+  const { title, author_id, publisher_id, genre_id, pub_year, isbn } = req.body;
+
+  const query = `
+    INSERT INTO books (title, author_id, publisher_id, genre_id, pub_year, isbn)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *;
+  `;
+
+  const values = [title, author_id, publisher_id, genre_id, pub_year, isbn];
+  const { rows } = await pool.query(query, values);
+
+  res.status(201).json(rows[0]);
+});
+
+app.put("/books/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, author_id, publisher_id, genre_id, pub_year, isbn } = req.body;
+
+  const query = `
+    UPDATE books
+    SET title = $1,
+        author_id = $2,
+        publisher_id = $3,
+        genre_id = $4,
+        pub_year = $5,
+        isbn = $6
+    WHERE book_id = $7
+    RETURNING *;
+  `;
+
+  const values = [title, author_id, publisher_id, genre_id, pub_year, isbn, id];
+  const { rows } = await pool.query(query, values);
+
+  if (rows.length === 0)
+    return res.status(404).json({ error: "Book not found" });
+  res.json(rows[0]);
+});
+
+app.delete("/books/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const query = "DELETE FROM books WHERE book_id = $1 RETURNING *";
+  const { rows } = await pool.query(query, [id]);
+
+  if (rows.length === 0)
+    return res.status(404).json({ error: "Book not found" });
+
+  res.json({ message: "Book successfully deleted", livro: rows[0] });
+});
+
+app.listen(3000, () => console.log("The API is running on port 3000"));
